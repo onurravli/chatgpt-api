53190313c3f25cffcf4a44140cebbfcb714053eb
onurravli <onurravli@hotmail.com>
2023-04-01 04:56:37 +0300

    Remove dotenv

--- demo.py
+++ demo.py
@@ -1,8 +1,6 @@
 from chatgpt import ChatGPT
-from dotenv import load_dotenv # Just for env. variables.
 import os
 
-load_dotenv()
-gpt = ChatGPT(str(os.getenv("KEY")))
+gpt = ChatGPT(os.getenv("OPENAI_KEY"))
 while True:
     gpt.chat()
